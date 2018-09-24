f075529813a4c33579ebf75330c2133a34103b3f
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 12:46:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,11 @@
 class CashRegister
   attr_accessor :total,
 
-  def initialize(total, employee_discount)
+  def initialize(total = 0, employee_discount)
     @total = total
   end
 
-  def self.total(total, employee_discount)
-    @total = total
+  def self.total
   end
 
   def add_item(title, price)

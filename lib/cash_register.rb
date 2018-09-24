db2f46581a83336bf73a1f972a55cc518c01bafe
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 17:58:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     @price = price
     @title = title
     self.total = @total + (price*amount)
-    @all_items << self.title
+    @all_items << self.title * amount
   end
 
   def apply_discount

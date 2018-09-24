de7538b98c50ce5eba402b47b52fbf483a48e9f3
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 12:43:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister 
 
+  def total(employee_discount = 0)
+  end
+
+  def add_item(title, price)
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end

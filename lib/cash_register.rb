7ef454c9a51bf3163a4879b22d9b9e436b869896
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 13:43:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,6 +21,7 @@ class CashRegister
     discount_float = @discount.to_f
     self.total = @price - (discount_float/100 * float_price)
     self.total = self.total.to_i
+    binding.pry
     return "After the discount, the total comes to $#{self.total}."
   end
 
